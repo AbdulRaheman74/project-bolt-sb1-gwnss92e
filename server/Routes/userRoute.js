dc4f5ccd8672b5216642894720afd8ee40f9d144
Abdul Raheman <abraheman.744@gmail.com>
2025-09-29 07:13:46 -0700

    controllerchanges

--- server/Routes/userRoute.js
+++ server/Routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUserProfile, loginUser, registerUser} from "../Controllers/userController.js";
+import { getUserProfile, loginUser, registerUser} from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const userRoute = express.Router();
